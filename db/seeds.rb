5a737b209fe76a52067046a1fed6b89c090807ee
raoakif <akifrao@gmail.com>
2022-08-10 17:20:22 +0500

    migrations files edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-['Hello', 'Hi', 'Hey', 'Howdy', 'Greetings'].each do |greeting|
+['Salam, Hello', 'Hi', 'Hey', 'Greetings'].each do |greeting|
   Greeting.create(message: greeting)
 end
